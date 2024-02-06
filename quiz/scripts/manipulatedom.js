f6c4d3fb705c5c3402203baec2be9374f9105cbf
wdelcol12 <wdelcol@gmail.com>
2024-02-05 19:52:52 -0500

    activities

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -7,6 +7,8 @@ function createTRNode(colNodes) {
   colNodes.forEach(function(colNode) {
     trNode.appendChild(colNode);
   })
+
+
   return trNode;
 }
 
@@ -21,11 +23,27 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createBtnNode(text, x, y) {
+  let btnNode = document.createElement("button");
+  let btnNodeTxt = createTxtNode(text);
+  btnNode.appendChild(btnNodeTxt);
+  btnNode.addEventListener(x, y);
+  return btnNode;
+}
+function edit(node) {
+  let replacementNode = document.createElement("input");
+  replacementNode.type = 'text';
+  replacementNode.placeholder = 'Enter Cell (x,y) ...';
+  node.replaceChild(replacementNode, node.childNodes[0]);
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
+
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(createBtnNode("Edit Text", "click", function() {edit(col1)}));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
